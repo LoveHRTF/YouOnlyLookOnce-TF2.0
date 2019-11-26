37e3ab5e4ad1e75923883a0801a735144a4bd768
FrozenFace <zwchen95@gmail.com>
2019-11-25 23:52:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import tensorflow as tf
 import numpy as np
+from src import dataset
+from src import model
+from src import train
+from src import test
+from src import config
 
 def main():
-    # Main function
-    pass
+    #TODO: Main function
+    # Preprocess 
+    dataset = Dataset(cfg.common_params, cfg.dataset_params['train_file'])
+    
+
+    #
+    model = Model()
+
+    # Train
+    for epoch in range(150):
+        # adjust lr, lr = 1e-3 for first 5 epochs
+        if epoch > 5:                   # next 10 epochs, slowly raise lr from 1e-3 to 1e-2
+            model.learning_rate += 1e-3
+        else if epoch > 15:             # next 75 epochs, lr = 1e-2
+            model.learning_rate = 1e-2
+        else if epoch > 90:             # next 30 epochs, lr = 1e-3
+            model.learning_rate = 1e-3
+        else if epoch > 120:            # next 30 epochs, lr = 1e-4
+            model.learning_rate = 1e-4
+        
+        train(model, inputs, labels)
+
+
+    # Test
+
+
+
 
 if __name__ == "__main__":
     pass
