88b7a2e19eb974b6b01fc77a2437c319095879d6
ZeyuRuan <43192778+ZeyuRuan@users.noreply.github.com>
2019-11-26 16:36:40 -0500

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,10 +1,22 @@
 import config as cfg
+import tensorflow as tf
+import dataset
 
-def train(self, model, images, labels):
+def train(self, model, dataset):
     '''
     Train model for one epoch. 
     '''
     batch_size = cfg.common_params['batch_size']
+    
+    for i in range(dataset.num_batch_per_epoch):
+        images, labels, record_point, num_batch_per_epoch = dataset.batch()
+        
+        with tf.GradientTape() as tape:
+            predictions = model.call(images)
+            loss = model.loss(predictions, labels)
+        
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     
     
     pass
