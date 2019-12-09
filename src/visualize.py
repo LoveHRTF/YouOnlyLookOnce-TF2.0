b22e21908bc97a7c4fa658da710dcefbcb4a3c90
LoveHRTF <zwchen95@gmail.com>
2019-12-08 21:31:55 -0500

    Fix visualization issue

--- src/visualize.py
+++ src/visualize.py
@@ -14,32 +14,44 @@ def visualization(model, img_path):
     :return: New image with bounding boxes and class names.
     """
     # bgr_mean = (103.939, 116.779, 123.68)  # bgr
-    # img = cv2.imread(img_path)
-    img = img_path
-
+    img = cv2.imread(img_path)
     # resize origin image
-    image_size = cfg.common_params['image_size']
+    image_size = int(cfg.common_params['image_size'])
     img = cv2.resize(img, (image_size, image_size))
-    img = tf.reshape(img, (1, image_size, image_size, 3))
-    # img = img - np.array(bgr_mean, dtype=np.float32)
 
-    logits = model(img)
+    # Forward Pass, prediction
+    model_input = tf.reshape(img, (-1, image_size, image_size, 3))
+    model_input = tf.dtypes.cast(model_input, tf.float32)
+    logits = model(model_input)
+    print(logits)
     boxes, class_idx, scores = decoder(logits, conf_thresh=0.1, score_thresh=0.1)
 
+    plt.imshow(img)
+    plt.show()
+
     for i in range(boxes.shape[0]):
-        x1 = boxes[i, 0]
-        y1 = boxes[i, 1]
-        x2 = boxes[i, 2]
-        y2 = boxes[i, 3]
+        x1 = int(boxes[i, 0])
+        y1 = int(boxes[i, 1])
+        x2 = int(boxes[i, 2])
+        y2 = int(boxes[i, 3])
         
         # draw a green rectangle to visualize the bounding box
-        image = cv2.rectangle(img, (x1, y1), (x2, y2), color=(0, 255, 0), thickness=2)
 
+        print("dfssaigheifashgisodfhgoisdfhgoisdfhgoisdhgosdhoi",x1)
+
+
+        start_point = (x1+30, y1+30)
+        end_point = (x2, y2)
+        color = (0, 255, 0)
+        thickness = 2
+        fontScale = 1
+
+        image = cv2.rectangle(img, start_point, end_point, color, thickness)
         # print class name (a green text)
-        class_name = cfg.class_names[class_idx[i]]
+        class_name = cfg.class_names[int(class_idx[i])]
         image = cv2.putText( 
-            image, class_name, (x1, y1), font=cv2.FONT_HERSHEY_SIMPLEX, 
-            fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA )
+            image, class_name, start_point, cv2.FONT_HERSHEY_SIMPLEX, 
+            fontScale, color, thickness, cv2.LINE_AA)
         
     cv2.imshow('show_result',image)
 
