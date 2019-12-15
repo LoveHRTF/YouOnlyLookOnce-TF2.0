809dae2966f10c1c7e38a6eca16a26312d6edf2e
FrozenFace <zwchen95@gmail.com>
2019-12-14 23:34:37 -0500

    Add files via upload

---
+++ setup_pascal_voc.sh
@@ -0,0 +1,27 @@
+# Automatically setup for VOC dataset
+pip install -r requirements.txt
+
+# Download PASCAL 2007/2012
+cd dataset
+wget https://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
+wget https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
+wget https://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
+
+# Unzip
+tar xf VOCtrainval_11-May-2012.tar
+tar xf VOCtrainval_06-Nov-2007.tar
+tar xf VOCtest_06-Nov-2007.tar
+
+# Generate PASCAL lists
+wget https://pjreddie.com/media/files/voc_label.py
+python voc_label.py
+
+# Generate PASCAL lists for YOLO
+cd ../tool
+python preprocess_pascal_voc.py
+
+# Finish
+cd ../src
+
+# Start Training
+python main.py
