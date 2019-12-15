9e36c55886c343eae4e9709381d524a410fd2044
LoveHRTF <zwchen95@gmail.com>
2019-12-15 00:05:07 -0500

    Create setup_coco.sh

---
+++ setup_coco.sh
@@ -0,0 +1,30 @@
+# Setup, this may take over an hour
+
+# mkdir
+mkdir data/coco
+mkdir data/coco/annotations
+mkdir data/coco/images
+
+# Data Download and Unzip
+cd data/coco/annotations
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+
+unzip annotations_trainval2017.zip
+
+cd ../images
+wget http://images.cocodataset.org/zips/train2017.zip
+wget http://images.cocodataset.org/zips/val2017.zip
+
+unzip train2017.zip
+unzip val2017.zip
+
+# Move coco.names to data
+mv ../../../tool/coco.names ../../../data/
+
+# Generate Label files
+cd ../../../tool
+python preprocess_cooc.py
+
+# Start training
+# cd ../src/
+# python main.py
