162d0d64a53e0e6e332de0e19fc8841b2cef5fdb
LoveHRTF <zwchen95@gmail.com>
2019-12-14 23:39:46 -0500

    Update setup_pascal_voc.sh

--- setup_pascal_voc.sh
+++ setup_pascal_voc.sh
@@ -2,7 +2,7 @@
 pip install -r requirements.txt
 
 # Download PASCAL 2007/2012
-cd dataset
+cd data
 wget https://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
 wget https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
 wget https://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
